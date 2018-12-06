80a4075060cc0e60423791768fe9aa6408c355a8
Nick Gard <nick.gard@nike.com>
2018-12-06 07:40:39 -0800

    Simplify internals

--- src/index.js
+++ src/index.js
@@ -3,37 +3,15 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.some = some;
 
-function some(iterable) {
-  const promises = Array.from(iterable);
-  const reasons = [];
-  let rejectionsCount = 0;
+function some(promises) {
+  return reverse(Promise.all(Array.from(promises).map(reverse)))
+}
 
-  if (promises.length === 0) {
-    return Promise.reject();
-  }
-
-  const nonPromise = promises.find(promise => !(promise instanceof Promise));
-  if (nonPromise) {
-    // resolve with the first non-Promise value
-    return Promise.resolve(nonPromise);
-  }
-
-  function accumulateRejection(reject, index) {
-    return function(reason) {
-      // rejection reasons should be in the same order as the promises that generated them
-      reasons[index] = reason;
-      if (++rejectionsCount === promises.length) {
-        // reject if all promises rejected
-        reject(reasons);
-      }
-    };
-  }
-
-  return new Promise(function(resolve, reject) {
-    Promise.race(
-      promises.map(function(promise, i) {
-        return promise.then(resolve, accumulateRejection(reject, i));
-      })
-    );
-  });
+function reverse(promise) {
+  return new Promise((resolve, reject) => {
+    if (promise && typeof promise.then === 'function') {
+      return promise.then(reject, resolve);
+    }
+    reject(promise);
+  })
 }
